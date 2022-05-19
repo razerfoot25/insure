c86cd9b207e61245c034bc9bd36703a13b7546ae
razerfoot25 <jasonaldueza@gmail.com>
2022-05-19 20:36:12 +0800

    2nd commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
 const menuIcon = document.querySelector(".nav__logo-container--menu-logo");
 const menu = document.querySelector(".nav__menu-container-bg");
 
-const disableOverflowX = () => {
-  document.body.style.overflowX = "hidden";
-};
-
 menuIcon.addEventListener("click", (e) => {
   menuIcon.classList.toggle("nav--open");
-
   menu.classList.toggle("menu--open");
 });
-
-window.onload = disableOverflowX;
